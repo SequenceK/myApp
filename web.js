f3b650f6ce7edf7f064b57d6a28964e0477313e9
Omar <SKing.omar@gmail.com>
2014-08-08 21:40:49 +0400

    Phaser

--- web.js
+++ web.js
@@ -1,15 +1,13 @@
 // web.js
-var express = require("express");
-var logfmt = require("logfmt");
+var express = require('express');
+var port = process.env.PORT || 5000;
 var app = express();
-
-app.use(logfmt.requestLogger());
-
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+app.get('/', function(request, response) {
+response.sendfile(__dirname + '/index.html');
 });
+app.use('/assets', express.static(__dirname + '/assets'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
-var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
-  console.log("Listening on " + port);
+console.log("Listening on " + port);
 });
